7c107be6d969846fa6db5d973306e0946bb1f206
Justin Driscoll <justin@driscolldev.com>
2013-10-09 09:33:46 -0400

    Clean up detail view a bit

--- DynamicType/DetailViewController.m
+++ DynamicType/DetailViewController.m
@@ -24,9 +24,11 @@
 
 - (void)updateStyle
 {
+    static NSString *sampleText = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus tellus, vulputate ac lobortis in, ornare placerat mi. In hac habitasse platea dictumst. Sed iaculis metus a tellus ullamcorper ultricies. Suspendisse in neque erat, at tincidunt enim. Aenean eros urna, semper sed blandit sed, vestibulum pulvinar arcu. Vestibulum vitae justo dolor. Etiam in turpis urna. Mauris blandit tincidunt scelerisque. In convallis velit quis risus vestibulum iaculis. Cras ullamcorper convallis dui, quis semper nisi iaculis vitae. Aliquam adipiscing, quam sed vehicula euismod, risus turpis dictum lorem, in bibendum ligula erat a arcu. Phasellus ac pretium ligula. Vivamus vestibulum pretium elit, et dignissim urna lacinia mattis. Sed eget tristique nisi. In hac habitasse platea dictumst. Maecenas elit dui, pretium quis accumsan at, mattis sit amet nulla. Phasellus nec tortor turpis. Sed mattis augue a mauris volutpat a convallis quam aliquet.";
+
     UIFont *font = [UIFont preferredFontForTextStyle:self.style];
     self.textView.font = font;
-    self.textView.text = [NSString stringWithFormat:@"%@\n\n%d", [font description], [font fontDescriptor].symbolicTraits];
+    self.textView.text = [NSString stringWithFormat:@"Family\n%@\n\nFont\n%@\n\nSize\n%f\n\nLine Height\n%f\n\n%@", font.familyName, font.fontName, font.pointSize, font.lineHeight, sampleText];
     self.title = self.style;
 }
 
